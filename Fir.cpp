b01e6a84dcba0ebcda656efeea6701103776daeb
Cheprichate <petrovich15071998@mail.ru>
2023-10-26 20:06:01 +0400

    Добавьте файлы проекта.

---
+++ Fir.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+int i, row, height;
+
+int main() {
+    setlocale(LC_ALL, "rus");
+
+    std::cout << "Введите высоту елочки: ";
+    std::cin >> height;
+
+    for (i = 0; i < height; i++)
+    {
+        for (row = 1; row < height - i; row++)
+        {
+            std::cout << " ";
+        }
+        for (row = height - 2 * i; row <= height; row++)
+        {
+            std::cout << "#";
+        }
+        std::cout << "\n";
+    }
+}
